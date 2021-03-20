9a7619eca9134adfc5b8fd814fd8c09ebe8204ca
BarghestEPL <raphael.wats@student.uclouvain.be>
2021-03-20 20:18:54 +0100

    server lag

--- proto.py
+++ proto.py
@@ -1,7 +1,7 @@
 import json
 
 
-def recv_msg(sock, h=1024, f="utf-8"):
+def recv_msg(sock, h=512, f="utf-8"):
     try:
         msg_len = int(sock.recv(h).decode(f))
         data = sock.recv(msg_len).decode(f)
@@ -12,9 +12,10 @@ def recv_msg(sock, h=1024, f="utf-8"):
         return None
 
 
-def send_msg(sock, data, h=1024, f="utf-8"):
+def send_msg(sock, data, h=512, f="utf-8"):
     msg = json.dumps(data).encode(f)
     msg_len = str(len(msg)).encode(f)
+    print(msg_len)
     try:
         sock.sendall(msg_len + (h - len(msg_len)) * b' ')
         sock.sendall(msg)
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import time
 import socket
 from proto import *
+import pygame as pg
 from GameObjects.GameObject import *
 
 HP = HOST, PORT = "", 45632
@@ -8,9 +9,13 @@ srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 srv.bind(HP)
 srv.listen(5)
 
-
+fps = 60
+dt = 1 / fps
+clock = pg.time.Clock()
 blue_sock, _ = srv.accept()
+print("blue is connected")
 red_sock, _ = srv.accept()
+print("red is connected")
 
 playerBlue = Player(blue_sock)
 playerRed = Player(red_sock)
@@ -25,3 +30,4 @@ while True:
 
     send_msg(playerRed.sock, data)
     send_msg(playerBlue.sock, data)
+    clock.tick(fps)
