4a56b063bbf63ce99a2fd3801d915d25d4950c31
BarghestEPL <raphael.wats@student.uclouvain.be>
2021-03-24 20:22:37 +0100

    need to fix windows bug

--- UDPV2/GameObjects.py
+++ UDPV2/GameObjects.py
@@ -1,15 +1,19 @@
 from const import *
 import pygame as pg
+from time import time
 from random import randint
 
 
+clients = {}
 gameObjects = []
 
 
 class Player:
-    def __init__(self, inputs):
+    def __init__(self, addr, inputs):
         gameObjects.append(self)
-        self.timeout =
+        self.addr = addr
+        self.timeout = time()
+
         self.inputs = inputs
         self.pos = pg.Vector2(400, 300)
         self.color = pg.color.Color(
@@ -24,19 +28,19 @@ class Player:
 
     def set_inputs(self, inputs):
         self.inputs = inputs
-        self.timeout = TIMEOUT_MAX
+        self.timeout = time()
 
     def update(self, dt):
-        if self.timeout > 0:
+        if time() - self.timeout < 1:
             if self.inputs["ml"]:
                 self.move_at = pg.Vector2(self.inputs["pos"])
                 self.step = (self.move_at - self.pos + pg.Vector2(0.01, 0.01)).normalize() * self.speed
 
             if self.pos.distance_to(self.move_at) > 10:
                 self.pos += self.step * dt
-            self.timeout -= 1
         else:
             gameObjects.remove(self)
+            clients.pop(self.addr)
 
     def get_state(self):
         return {
--- UDPV2/server.py
+++ UDPV2/server.py
@@ -6,7 +6,6 @@ from GameObjects import *
 
 dt = 0
 run = 1
-clients = {}
 clock = pg.time.Clock()
 srv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 srv.bind(("", PORT))
@@ -17,9 +16,9 @@ def handle_clients():
         data, addr = srv.recvfrom(DATA_LEN)
         player = clients.get(addr)
         if player:
-            player.inputs = json.loads(data.decode("utf-8"))
+            player.set_inputs(json.loads(data.decode("utf-8")))
         else:
-            clients[addr] = Player(json.loads(data.decode("utf-8")))
+            clients[addr] = Player(addr, json.loads(data.decode("utf-8")))
 
 
 threading.Thread(target=handle_clients, daemon=True).start()
